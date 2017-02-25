294bf25c9abd41a000dffc7bd390c6b69af1aa78
skyitachi <skbysp@gmail.com>
2017-02-25 10:30:50 +0800

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ currentUsr=`whoami`
 currentDir=`pwd`
 shell="/bin/bash"
 # parameter parsing
-function show_usage() {
+function show_usage {
   echo "must run with super-user privileges."
   echo -e "\nUsage: bootstrap.sh [-h] [-u user_name]"
 }
@@ -11,6 +11,17 @@ function show_usage() {
 function addUserPrompt {
   echo -n "please input home directory: "
   read homeDir
+}
+
+function setup_home_dir {
+  homeDirs=(bin utility lab personal resource)
+  for i in "${homeDirs[@]}"; do
+    if [ -d "$currentDir/${i}" ]; then
+      continue
+    else
+      mkdir $currentDir/${i}
+    fi
+  done
 }
 
 while [ "$#" -gt 0 ]; do
@@ -29,22 +40,8 @@ while [ "$#" -gt 0 ]; do
       homeDir=/home/$1
       addUserPrompt
       adduser $1 --home $homeDir --shell $shell
+      setup_home_dir
       shift
       ;;
   esac
 done
-
-#if [ $currentUsr != "root" ]; then
-#  echo "please run with root"
-#  exit 1
-#fi
-homeDirs=(bin utility lab personal resource)
-for i in "${homeDirs[@]}"; do
-  if [ -d "$currentDir/${i}" ]; then
-    continue
-  else
-    mkdir $currentDir/${i}
-  fi
-done
-echo $currentUsr
-
