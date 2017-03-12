1df509402dd72098d7645654957f27b1afba00f2
skyitachi <skbysp@gmail.com>
2017-03-12 08:47:58 +0800

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,6 +50,10 @@ while [ "$#" -gt 0 ]; do
       user=$1
       homeDir=/home/$1
       adduser $1 --home $homeDir --shell $shell
+      # if adduser fails cannot do setup
+      if [ $? -gt 0 ]; then
+        exit 1
+      fi
       setup_home_dir
       setup_sudoers
       shift
